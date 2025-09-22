8bbd321f87e7d6b9414560b142a78d1d85c56bf6
ConfusedDoggos <thesmilingman654@gmail.com>
2025-09-21 21:51:30 -0700

    FTClib Beginning to work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShooterTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShooterTest.java
@@ -1,43 +1,38 @@
 package org.firstinspires.ftc.teamcode;
 
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.drawCurrent;
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.drawCurrentAndHistory;
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.follower;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
+import com.seattlesolvers.solverslib.drivebase.MecanumDrive;
+import com.seattlesolvers.solverslib.gamepad.GamepadEx;
 import com.seattlesolvers.solverslib.hardware.motors.Motor;
 import com.seattlesolvers.solverslib.hardware.motors.MotorEx;
 
 @TeleOp(name = "ShootAS",group = "AS")
 public class ShooterTest extends OpMode {
-    public MotorEx shooter;
-    //DcMotor shooter1 = shooter.motorEx;
+    public MotorEx shooter, intake;
+    private MotorEx fL, fR, bL, bR;
+    private MecanumDrive drive;
+    private GamepadEx driverOp;
     @Override
     public void init() {
-        shooter = new MotorEx(hardwareMap,"shooter1", Motor.GoBILDA.BARE);
+        shooter = new MotorEx(hardwareMap,"shooterMotor", Motor.GoBILDA.BARE);
+        intake = new MotorEx(hardwareMap,"intakeMotor", Motor.GoBILDA.RPM_435);
+        fL = new MotorEx(hardwareMap,"fL",Motor.GoBILDA.RPM_312);
+        fR = new MotorEx(hardwareMap,"fR",Motor.GoBILDA.RPM_312);
+        bL = new MotorEx(hardwareMap,"bL",Motor.GoBILDA.RPM_312);
+        bR = new MotorEx(hardwareMap,"bR",Motor.GoBILDA.RPM_312);
+        drive = new MecanumDrive(fL, fR, bL, bR);
+        driverOp = new GamepadEx(gamepad1);
     }
 
-    /** This initializes the PoseUpdater, the mecanum drive motors, and the Panels telemetry. */
     @Override
     public void init_loop() {
     }
 
     @Override
     public void start() {
-        if (gamepad1.a) {
-            shooter.set(0.5);
-        }
-        if (gamepad1.b) {
-            shooter.set(-0.5);
-        }
-        if (gamepad1.x) {
-            shooter.set(0.85);
-        }
-        if (gamepad1.y) {
-            shooter.set(-0.85);
-        }
+
     }
 
     /**
@@ -46,6 +41,30 @@ public class ShooterTest extends OpMode {
      */
     @Override
     public void loop() {
-
+        drive.driveRobotCentric(
+                -driverOp.getLeftX(),
+                -driverOp.getLeftY(),
+                -driverOp.getRightX()
+        );
+        if (gamepad1.a) {
+            shooter.set(0.5);
+            telemetry.addData("A",gamepad1.a);
+        } else if (gamepad1.b) {
+            shooter.set(-0.5);
+            telemetry.addData("B",gamepad1.b);
+        } else {
+            shooter.set(0);
+        }
+        if (gamepad1.x) {
+            intake.set(1);
+            telemetry.addData("X",gamepad1.x);
+        } else if (gamepad1.y) {
+            intake.set(-1);
+            telemetry.addData("Y",gamepad1.y);
+        } else {
+            intake.set(0);
+        }
+        telemetry.addData("motor velocity",shooter.getVelocity());
+        telemetry.update();
     }
 }
