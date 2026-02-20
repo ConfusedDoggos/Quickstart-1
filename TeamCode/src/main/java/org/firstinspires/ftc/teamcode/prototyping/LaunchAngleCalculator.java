976af13b41576d37321db965269721b67f51d84d
Nason McGillin <nason321@yahoo.com>
2026-02-19 19:18:50 -0800

    Improved high vs low angle reasoning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/prototyping/LaunchAngleCalculator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/prototyping/LaunchAngleCalculator.java
@@ -8,8 +8,10 @@ public class LaunchAngleCalculator {
     InterpLUT angleLUT = new InterpLUT();
     //height is in meters
     int height = 3;
-    double minAngle = 35;
-    double maxAngle = 60;
+    double minHoodAngle = 35;
+    double maxHoodAngle = 60;
+
+    double closeDistance = .1;
     private void createAngleLUT() {
         //for mapping angles (in degrees) to servo positions on the hood
         angleLUT.add(0,0);
@@ -22,17 +24,36 @@ public class LaunchAngleCalculator {
     }
     public double calcBestAngle(double velocity, double distance) {
 
-        //didnt change much here, just made it easier to read (It does work). distance is meters and angles in radians
+        //didn't change much here, just made it easier to read (It does work). distance is meters and angles in radians
         double sqrt = Math.sqrt(Math.pow(velocity,4) - (9.8 * ((9.8 * Math.pow(distance, 2)) + (2 * height * Math.pow(velocity,2)))));
         double vel_1 = Math.pow(velocity,2);
         double divider = 9.8 * distance;
         
-        double minAngle = Math.atan((vel_1 - sqrt)/divider);
-        double maxAngle = Math.atan((vel_1 + sqrt)/divider);
+        double lowAngle = Math.atan((vel_1 - sqrt)/divider);
+        double highAngle = Math.atan((vel_1 + sqrt)/divider);
 
-        if ((lowerAngle * 180/Math.PI) > minAngle && (upperAngle * 180/Math.PI) < maxAngle) return angleLUT.get(lowerAngle * 180/Math.PI);
-        else if ((upperAngle * 180/Math.PI) > minAngle && (upperAngle * 180/Math.PI) < maxAngle) return angleLUT.get(upperAngle * 180/Math.PI);
-        else return currentAngle;
+        //converts hood angle into degrees and then into servo positions
+        lowAngle = angleLUT.get(lowAngle * (180/Math.PI));
+        highAngle = angleLUT.get(highAngle * (180/Math.PI));
 
+
+
+        if (lowAngle > minHoodAngle && lowAngle < maxHoodAngle && highAngle > maxHoodAngle || highAngle < minHoodAngle) {
+            return lowAngle;
+        }
+        else if (highAngle > minHoodAngle && highAngle < maxHoodAngle && lowAngle > maxHoodAngle || lowAngle < maxHoodAngle) {
+            return highAngle;
+        }
+        else if (highAngle > maxHoodAngle || highAngle < minHoodAngle && lowAngle > minHoodAngle || lowAngle > maxHoodAngle) {
+            if (distance <= closeDistance) {
+                return highAngle;
+            }
+            else {
+                return lowAngle;
+            }
+        }
+        else {
+            return -1;
+        }
     }
 }
